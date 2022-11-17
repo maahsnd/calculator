35ea0cc1810a16d39552f7b36bfa309baa499291
Replit user <>
2022-11-17 17:17:47 +0000

    Create JS foundation for calculator
    Write functions for basic math operators, operate function to
    call them.

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(addend,addend1) {
+    return (addend + addend1);
+}
+
+function subtract(minuend, subtrahend) {
+    return (minuend - subtrahend);
+}
+
+function multiply(multipicand, multiplier) {
+    return (multipicand * multiplier);
+}
+
+function divide(dividend, divisor) {
+    return( dividend / divisor);
+}
+
+function operate(operator, operand, operand1) {
+    if (operator === add) {
+        return add(operand, operand1);
+    }
+
+    else if (operator === subtract) {
+        return subtract(operand, operand1);
+    }
+
+    else if (operator === multiply) {
+        return multiply(operand, operand1);
+    }
+
+    else {
+        return divide(operand, operand1);
+    }
+}
